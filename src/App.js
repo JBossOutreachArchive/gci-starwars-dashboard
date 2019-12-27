3a2e6b35c0ac00cd0c0cdc851d9ebb92f9199355
anukritijha <anukritijha@users.noreply.github.com>
2019-12-27 22:09:23 +0530

    Revert "Added Starwars Dashboard with pagination"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MainPage from './components/MainPage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="dashHead">
-           <b>DASHBOARD </b>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <hr/>
-      <br/>
-      <p className="head">
-        <b>Starwars Data</b>
-      </p>
-      <MainPage/>
     </div>
   );
 }
